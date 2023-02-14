799b3d7ef8ee63cb5df790093df1d9b19034cf81
Davi J <davijosantos.dj@gmail.com>
2023-02-14 09:15:09 -0300

    Style: modificação nos estilos das páginas e adição de icones

--- src/pages/myProducts/myProducts.tsx
+++ src/pages/myProducts/myProducts.tsx
@@ -1,62 +1,91 @@
-import React, { useState } from 'react'
-import { Text, View, TouchableOpacity, StyleSheet } from 'react-native'
-import BackButton from '../../components/BackButton/index'
-
+import React, { useState, useRef } from "react";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  ScrollView,
+} from "react-native";
+import BookToSell from "../../components/BookToSell";
+import BackButton from "../../components/BackButton/index";
 
 export function MyProducts({ navigation }) {
+  const [views, setViews] = useState([]);
+  const scrollViewRef = useRef(null);
 
-    const [views, setViews] = useState([]);
-
-    return (
-        <View style={{backgroundColor: '#F4EEA9', height: '100%', alignItems: 'center'}}>
-            <View style={styles.headerBox}>
-                <TouchableOpacity onPress={() => navigation.goBack()}>
-                    <BackButton/>
-                </TouchableOpacity>
-            </View>
-
-            <View>
-                {views}
-            </View>
-
-            <TouchableOpacity
-            style={styles.addLivro}
-            onPress={() => setViews([...views,
-                <View key={views.length} style={{ backgroundColor: 'yellow', width: 400, borderBottomWidth: 2,}}>
-                <Text>Livro {views.length + 1}</Text>
-                <Text>Autor do livro / Nenhum</Text>
-                </View>
-            ])}>    
-                <Text style={styles.textAdd}>Adicionar Livro</Text>
-            </TouchableOpacity>
-
-        </View>
-    )
+  return (
+    <View
+      style={{
+        backgroundColor: "#e6e0a1",
+        height: "100%",
+        width: "100%",
+        alignItems: "center",
+      }}
+    >
+      <View style={styles.headerBox}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <BackButton />
+        </TouchableOpacity>
+      </View>
+      <ScrollView
+        contentContainerStyle={styles.contentContainer}
+        ref={scrollViewRef}
+        style={{ width: "100%" }}
+        onContentSizeChange={() =>
+          scrollViewRef?.current?.scrollToEnd({ animated: true })
+        }
+      >
+        <View>{views}</View>
+        <TouchableOpacity
+          style={styles.addLivro}
+          onPress={() =>
+            setViews([
+              ...views,
+              <View key={views.length} style={{alignItems: 'center'}}>
+                <BookToSell/>
+              </View>,
+            ])
+          }
+        >
+          <Text style={styles.textAdd}>Adicionar Livro</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    headerBox: {
-        backgroundColor: '#16262E',
-        justifyContent: 'flex-start',
-        width: '100%'
-    },
+  headerBox: {
+    backgroundColor: "#16262E",
+    justifyContent: "flex-start",
+    width: "100%",
+  },
 
-    addLivro: {
-        width: 200,
-        height: 80,
-        
-        margin: 20,
-        padding: 20,
-        justifyContent: 'center',
-        alignItems: 'center',
-        
-        borderRadius: 15,
-        elevation: 2,
-        backgroundColor: '#D9D9D9'
-    },
+  addLivro: {
+    width: 250,
+    height: 60,
 
-    textAdd: {
-        fontSize: 18,
-        fontWeight: 'bold',
-    }
-})
+    margin: 20,
+    padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    borderRadius: 15,
+    elevation: 2,
+    backgroundColor: "#E7E9D9",
+
+    position: 'relative',
+  },
+
+  contentContainer: {
+      justifyContent: 'center',
+      alignItems: "center",
+  },
+
+  textAdd: {
+    fontSize: 18,
+    color: '#383235',
+    fontWeight: "bold",
+  },
+});
--- src/theme/colors.ts
+++
@@ -1,5 +0,0 @@
-const white = {
-  primary: "#f0f",
-  secondary: "#F4EEA9",
-  primaryDark: "#990099",
-};
