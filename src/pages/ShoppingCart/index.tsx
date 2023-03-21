7bf4a1fe96326276605c26b02f84d3dd30d1b2b3
Davi J <davijosantos.dj@gmail.com>
2023-03-21 09:47:27 -0300

    #22 header do carrinho de compras

--- src/pages/ShoppingCart/index.tsx
+++ src/pages/ShoppingCart/index.tsx
@@ -1,10 +1,33 @@
 import React, { useState } from "react"
-import { View, TextInput, Text } from "react-native"
+import { View, ScrollView, TextInput, Text, TouchableOpacity, Image, ImageBackground } from "react-native"
 import styles from "./styles";
 
-export function ShoppingCart(){
+export function ShoppingCart({ navigation }){
+    const img = [
+        {
+            uri: 'https://th.bing.com/th/id/OIP.knGOtQhXQcQr5yfBYSRmRAHaEK?pid=ImgDet&rs=1'
+        },
+        
+        {
+            uri: "https://http2.mlstatic.com/livro-harry-potter-e-as-reliquias-da-morte-ed-econmica--D_NQ_NP_757301-MLB20312596696_062015-F.jpg"
+        },
+        {
+            uri: "https://th.bing.com/th/id/OIP.cHvwABogsVcbK-z6dJEYrAHaKi?pid=ImgDet&rs=1"
+        },
+        {
+            uri: "https://th.bing.com/th/id/OIP.ASOA1yrBSoU9AbIlZIc52QHaKm?pid=ImgDet&rs=1"
+        },
+    ]
+
     return(
         <View>
+            <ScrollView>
+                <ImageBackground source={img[0]} resizeMode='cover' style={styles.headerImgStore} imageStyle={{height: "100%"}}>
+                        <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
+                            <Image source={require('../../../assets/BackIconNormal.png')}/>
+                        </TouchableOpacity>
+                    </ImageBackground>
+            </ScrollView>
             
         </View>
     );
--- src/pages/ShoppingCart/styles.tsx
+++ src/pages/ShoppingCart/styles.tsx
@@ -3,7 +3,34 @@ import { Nunito_400Regular } from '@expo-google-fonts/nunito'
 import themes from "../../themes"
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: themes.colors.tela.primaryBackground,
 
+    },
+
+    scrollContainer: {
+        // alignItems: 'center',
+        flexGrow: 1,
+        width: "100%",
+        height: '120%',
+    },
+
+    headerImgStore: {
+        // flex: 1,
+        backgroundColor: "#565",
+        width: "100%",
+        height: "16%",
+    },
+    
+    backButton: {
+        backgroundColor: "#D9D9D950",
+        justifyContent: "center",
+        width: 80,
+        height: "50%",
+
+        padding: 15,
+    },
 })
 
 export default styles
