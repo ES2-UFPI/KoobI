51e83bcd91b5d345b6aa2f23d08ad7a1f3caf6d9
Davi J <davijosantos.dj@gmail.com>
2023-03-21 22:36:19 -0300

    #20 concertando o tamanho dos ícones do catálogo

--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -27,34 +27,12 @@ export function Catalog( {navigation} ) {
             </View>
         </TouchableOpacity>,
 
-        <TouchableOpacity key="view3" style={styles.elementProduct} onPress={() => navigation.navigate('Loja')}>
-            <Image
-            style={{flex: 1, borderTopLeftRadius: 15, borderTopRightRadius: 15}}
-            source={{uri: 'https://almeidajunior-prod1.s3.amazonaws.com/prod/media_cache/store_picture_file_full/57c4a049b26dc.JPG'}}
-            />
-            <View style={styles.viewElementProduct}>
-                <Text style={styles.textLinksLibs}>Livraria Catarinense</Text>
-                <Text style={[styles.textLinksLibs, {textAlign: 'right', color: "#515050"}]}>{} Nº de títulos</Text>
-            </View>
-        </TouchableOpacity>,
-
-        <TouchableOpacity key="view4" style={styles.elementProduct} onPress={() => navigation.navigate('Loja')}>
-            <Image
-            style={{flex: 1, borderTopLeftRadius: 15, borderTopRightRadius: 15}}
-            source={{uri: 'https://th.bing.com/th/id/R.d5e5a57943df2897121c9426fd9f55f3?rik=mXxseMSptlMhOw&riu=http%3a%2f%2fwww.myownportugal.com%2fwp-content%2fuploads%2f2015%2f08%2fLIVRARIA-ARQUIVO_My-Own-Portugal3.jpg&ehk=sDIJqGzLZASool1vX3RcrmeVkjf53WgDMGRGWWhslds%3d&risl=&pid=ImgRaw&r=0'}}
-            />
-            <View style={styles.viewElementProduct}>
-                <Text style={styles.textLinksLibs}>Nome da Livraria</Text>
-                <Text style={[styles.textLinksLibs, {textAlign: 'right', color: "#515050"}]}>{} Nº de títulos</Text>
-            </View>
-        </TouchableOpacity>,
     ];
 
     return (
         <View style={styles.principalPage}>
             <ScrollView contentContainerStyle={styles.scrollOfElements}>
             <Header/>
-                
                 
                 {views}
 
