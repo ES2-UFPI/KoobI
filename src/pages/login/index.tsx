e3c27666da846d74a9bcadb4cb7164e0d1d25867
Rian Galatas Macedo Brandao <58229572+rianteam22@users.noreply.github.com>
2023-03-23 08:18:28 -0300

    #26

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,8 +1,5 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
-
 import login from "../../services/login"
-
+/*
 const LoginScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -11,43 +8,4 @@ const LoginScreen = () => {
     console.log(email,password)
     login(email, password)
     }
-
-  return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry={true}
-      />
-      <Button
-        title="Login"
-        onPress={handleLogin}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  input: {
-    height: 40,
-    width: '80%',
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  },
-});
-
-export default LoginScreen;
+*/
--- src/services/deleteBook.tsx
+++ src/services/deleteBook.tsx
@@ -1,12 +1,11 @@
 import { doc, deleteDoc } from "firebase/firestore";
 import { db } from "./firebaseConfig";
 
-async function deleteBook(text) {
+export default async function deleteBook(text) {
 
   const bookRef = doc(db, "livros", text);
   
   try{
-    console.log(bookRef);
     await deleteDoc(bookRef);
     console.warn(bookRef, " => foi deletado");
   } catch (e) {
@@ -14,5 +13,3 @@ async function deleteBook(text) {
   }
 
 }
-
-export default deleteBook;
