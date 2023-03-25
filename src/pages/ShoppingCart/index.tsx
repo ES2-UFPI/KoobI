ce60591ac328d127f26354788f0cc67768cc9ec2
Davi J <davijosantos.dj@gmail.com>
2023-03-25 17:32:13 -0300

    Some fixes in components

--- src/pages/ShoppingCart/index.tsx
+++ src/pages/ShoppingCart/index.tsx
@@ -5,26 +5,12 @@ import styles from "./styles";
 export function ShoppingCart({ navigation }){
     const [valorTotal, setValorTotal] = useState(0)
 
-    const img = [
-        {
-            uri: 'https://th.bing.com/th/id/OIP.knGOtQhXQcQr5yfBYSRmRAHaEK?pid=ImgDet&rs=1'
-        },
-        
-        {
-            uri: "https://http2.mlstatic.com/livro-harry-potter-e-as-reliquias-da-morte-ed-econmica--D_NQ_NP_757301-MLB20312596696_062015-F.jpg"
-        },
-        {
-            uri: "https://th.bing.com/th/id/OIP.cHvwABogsVcbK-z6dJEYrAHaKi?pid=ImgDet&rs=1"
-        },
-        {
-            uri: "https://th.bing.com/th/id/OIP.ASOA1yrBSoU9AbIlZIc52QHaKm?pid=ImgDet&rs=1"
-        },
-    ]
+    const img = { uri: 'https://th.bing.com/th/id/OIP.knGOtQhXQcQr5yfBYSRmRAHaEK?pid=ImgDet&rs=1'}
 
     return(
         <View style={styles.container}>
             <ScrollView contentContainerStyle={styles.scrollContainer}>
-                <ImageBackground source={img[0]} resizeMode='cover' style={styles.headerImgStore} imageStyle={{height: "100%"}}>
+                <ImageBackground source={img} resizeMode='cover' style={styles.headerImgStore} imageStyle={{height: "100%"}}>
                     <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
                         <Image source={require('../../../assets/BackIconNormal.png')}/>
                     </TouchableOpacity>
